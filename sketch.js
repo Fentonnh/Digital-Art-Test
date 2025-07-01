0e2673941b5b1950df8de6cd01450d4815ac5a1a
Fentonnh <fentonnh@gmail.com>
2025-07-01 01:23:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,17 @@
-let zoff = 0;
-let speedSlider;
-let speed = 0.01;
+let theShader;
+
+function preload() {
+  theShader = loadShader('shader.vert', 'shader.frag');
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  pixelDensity(1);
-  noSmooth();
-  const container = select('#speed-container');
-  speedSlider = createSlider(0.001, 0.1, speed, 0.001);
-  speedSlider.parent(container);
-  speedSlider.style('width', '200px');
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+  shader(theShader);
 }
 
 function draw() {
-  loadPixels();
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      let n = noise(x * 0.01, y * 0.01, zoff);
-      let c = n > 0.5 ? 255 : 0;
-      let index = (x + y * width) * 4;
-      pixels[index] = c;
-      pixels[index + 1] = c;
-      pixels[index + 2] = c;
-      pixels[index + 3] = 255;
-    }
-  }
-  updatePixels();
-  zoff += 0.01;
-  zoff += speedSlider.value();
+  theShader.setUniform("resolution", [width, height]);
+  theShader.setUniform("time", millis() / 1000.0);
+  rect(0, 0, width, height);
 }
