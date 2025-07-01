ea4347d653d8574ba5f50560175785cbde009a3e
Fentonnh <fentonnh@gmail.com>
2025-07-01 02:05:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,15 +8,14 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
   shader(theShader);
-  rectMode(CENTER); // Important!
 }
 
 function draw() {
   theShader.setUniform("resolution", [width, height]);
   theShader.setUniform("time", millis() / 1000.0);
 
-  // Fullscreen rectangle (centered in WebGL)
-  rect(0, 0, width, height);
+  // This will map the shader across a full-screen quad in WebGL space
+  plane(width, height);
 }
 
 function windowResized() {
