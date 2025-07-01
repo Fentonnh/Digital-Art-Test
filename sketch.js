caa51a3d3c33fe0f0da774538850076743de13f6
Fentonnh <fentonnh@gmail.com>
2025-07-01 00:49:59 -0400

    Add sketch.js for noise animation

--- sketch.js
+++ sketch.js
@@ -1,1 +1,28 @@
-
++let z = 0;
+ 
++function setup() {
++  createCanvas(windowWidth, windowHeight);
++  pixelDensity(1);
++  noStroke();
++}
++
++function draw() {
++  loadPixels();
++  for (let x = 0; x < width; x++) {
++    for (let y = 0; y < height; y++) {
++      let n = noise(x * 0.01, y * 0.01, z);
++      let c = n * 255;
++      let index = (x + y * width) * 4;
++      pixels[index] = c;
++      pixels[index + 1] = c;
++      pixels[index + 2] = c;
++      pixels[index + 3] = 255;
++    }
++  }
++  updatePixels();
++  z += 0.01;
++}
++
++function windowResized() {
++  resizeCanvas(windowWidth, windowHeight);
++}
