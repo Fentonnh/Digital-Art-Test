1281df5a17e66f9d538b0a0440b37e8f904b2175
Fentonnh <fentonnh@gmail.com>
2025-07-01 02:01:14 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,14 +14,14 @@ function draw() {
   theShader.setUniform("resolution", [width, height]);
   theShader.setUniform("time", millis() / 1000.0);
 
-  // Manually draw a full-screen quad using clip space coordinates
   beginShape();
-  vertex(-1, -1, 0);  // bottom left
-  vertex(1, -1, 0);   // bottom right
-  vertex(1, 1, 0);    // top right
-  vertex(-1, 1, 0);   // top left
+  vertex(-1, -1, 0);
+  vertex(1, -1, 0);
+  vertex(1, 1, 0);
+  vertex(-1, 1, 0);
   endShape(CLOSE);
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
